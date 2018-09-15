0de9292b149d2e414c97b2d75211470c8a995c50
Paul Clark <clark.paul123@gmail.com>
2018-09-15 16:48:30 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -19,6 +19,8 @@ function whileLoop(number) {
 
 function doWhileLoop(array) {
   function incrementVariable() {
+  }
+  do {
       array.pop();
     }
     while (array.length > 0 && incrementVariable());
