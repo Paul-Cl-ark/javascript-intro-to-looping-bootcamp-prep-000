24db322d8872b1d6e38bd8a7c35e15585b9912d6
Paul Clark <clark.paul123@gmail.com>
2018-09-15 16:50:46 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,17 +1,17 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
     if (i === 1) {
-    array.push("I am 1 strange loop.")
+    array.push("I am 1 strange loop.");
     } else {
-    array.push(`I am ${i} strange loop${i === 0 ? '' : 's'}.`)
+    array.push(`I am ${i} strange loop${i === 0 ? '' : 's'}.`);
     }
   }
-  return array
+  return array;
 }
 
 function whileLoop(number) {
   while (number > 0) {
-    console.log(-- number)
+    console.log(-- number);
   }
   if (number === 0) {
   return "done"}
@@ -23,6 +23,6 @@ function doWhileLoop(array) {
   do {
       array.pop();
     }
-    while (array.length > 0 && incrementVariable());
-    return array;
+  while (array.length > 0 && incrementVariable());
+  return array;
 }
